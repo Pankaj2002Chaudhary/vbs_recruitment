e58bb2f844b77e645c75a04e75055dff90d7b29e
Pankaj2002Chaudhary <pankajchaudhary6643@gmail.com>
2024-09-19 10:38:19 +0530

    change in template

--- recruitment_portal/models.py
+++ recruitment_portal/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Candidate(models.Model):
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    email=models.EmailField()
+    phone=models.IntegerField()
+    inteviewer=models.CharField(max_length=100)
+    resume=models.FileField()
+    experience=models.TextField()
+    address=models.TextField()
+    tech_stack=models.TextField()

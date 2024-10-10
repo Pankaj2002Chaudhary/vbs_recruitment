25b1778f8f9d9d44cdf2f84e26551e4c6cf32bf8
SarandeepSingh17 <sandysaluja7@gmail.com>
2024-10-10 15:04:03 +0530

    Refactoring code

--- recruitment_portal/models.py
+++ recruitment_portal/models.py
@@ -27,32 +27,5 @@ class Feedback(models.Model):
     def __str__(self):
         return f"Feedback for {self.candidate.name} by {self.interviewer}"
 
-
-
-
-
-
-
-
-
-# from django.db import models
-# from django.contrib.auth.models import User
-# # Create your models here.
-# class Candidate(models.Model):
-#     id=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')   
-#     name=models.CharField(max_length=100,default="None")
-#     age=models.BigIntegerField()
-#     email=models.EmailField()
-#     phone=models.BigIntegerField()
-#     registerdate=models.DateField(default='2024-01-01')
-#     interviewer=models.CharField(max_length=100)
-#     # resume=models.FileField(default='none')
-#     resume = models.FileField(upload_to='resumes/', blank=True, null=True)
-#     experience=models.TextField()
-#     address=models.TextField()
-#     tech_stack=models.TextField()
-
-#     def __str__(self):
-#         return self.name
     
 
